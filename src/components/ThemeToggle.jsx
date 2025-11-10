9089f6007175d1b61263a6191ea15dbb4b154198
BirukL137 <buraf13@gmail.com>
2025-11-10 14:07:10 +0300

    Theme Toggle Component

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,14 +1,17 @@
-import { useTodos } from "../context/TodosContext";
+import useTodos from "../hooks/useTodos";
 
 export default function ThemeToggle() {
   const { theme, dispatch } = useTodos();
+  const iconSrc =
+    theme === "light" ? "/assets/icon-moon.svg" : "/assets/icon-sun.svg";
+
   return (
     <button
       className="theme-toggle"
       onClick={() => dispatch({ type: "toggleTheme" })}
       aria-label="Toggle theme"
     >
-      {theme === "light" ? "🌙" : "☀️"}
+      <img src={iconSrc} alt="Toggle theme" />
     </button>
   );
 }
