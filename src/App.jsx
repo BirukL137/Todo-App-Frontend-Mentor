dfdbe11bc9afe875fc57db8ee43c362c0b5c8da3
BirukL137 <buraf13@gmail.com>
2025-11-10 14:08:34 +0300

    Updated App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
-import Filters from "./components/Filters";
 import ThemeToggle from "./components/ThemeToggle";
 import TodoFooter from "./components/TodoFooter";
+import useTodos from "./hooks/useTodos";
 
 function App() {
+  const { todos } = useTodos();
   return (
     <div className="app">
+      <div className="app-bg__image"></div>
       <header className="header">
-        <h1 className="header__title">TODO</h1>
+        <h1 className="header-title">TODO</h1>
         <ThemeToggle />
       </header>
 
       <main className="todo">
         <TodoInput />
-        <div className="todo__panel">
-          <TodoList />
-          <Filters />
-          <TodoFooter />
-        </div>
+        {todos.length > 0 ? (
+          <>
+            <div className="todo-panel">
+              <TodoList />
+              <TodoFooter />
+            </div>
 
-        <p className="todo__hint">Drag & drop to reorder list</p>
+            <p className="todo-hint">Drag & drop to reorder list</p>
+          </>
+        ) : (
+          <p className="todo-empty">No task yet, add new one!</p>
+        )}
       </main>
     </div>
   );
