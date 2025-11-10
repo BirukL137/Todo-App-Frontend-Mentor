038962f3a4f0022d625b5999d07ebfb7196aae34
BirukL137 <buraf13@gmail.com>
2025-11-10 14:21:41 +0300

    Main Component Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./styles/base.css";
 import "./styles/layout.css";
 import "./styles/components.css";
+import App from "./App.jsx";
 import { TodosProvider } from "./context/TodosContext.jsx";
 
 createRoot(document.getElementById("root")).render(
